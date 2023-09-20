69ab58a63782c542ef313f073fd64cc454ad6de8
Vanvrs <107437663+Vanvrs@users.noreply.github.com>
2023-09-20 09:48:52 -0300

    Lista_atividades
    Lista de Atividades com reactJs

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Listagem from './Listagem'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Listagem />
+  </React.StrictMode>,
+)
